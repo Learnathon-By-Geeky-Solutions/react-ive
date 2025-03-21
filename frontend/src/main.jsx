ed255196b5224811c17460c6df02067463ddb9ce
CodeWithIsmail <hmismail360@gmail.com>
2025-03-21 23:20:42 +0600

    app.js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import {SocketContextProvider} from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <AuthProvider>
+        <SocketContextProvider>
+          <App />
+        </SocketContextProvider>
+      </AuthProvider>
+    </Router>
   </StrictMode>,
 )
