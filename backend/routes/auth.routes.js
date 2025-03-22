1f39c89d9195730e3de3111cc2a1d4096e49f559
Tashrif-007 <tashrifpro@gmail.com>
2025-03-22 22:26:32 +0600

    reset password functionality api added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, register } from '../controllers/auth.controller.js';
+import { login, register, sendMail, resetPassword } from '../controllers/auth.controller.js';
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
+authRouter.post("/sendMail", sendMail);
+authRouter.post("/resetPassword", resetPassword);
 
 export default authRouter;
