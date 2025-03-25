67f8f90a2fb01c029c28415a257b9c04c5d8436b
Tashrif-007 <tashrifpro@gmail.com>
2025-03-25 22:19:48 +0600

    get and delete  message api tested

--- backend/routes/message.routes.js
+++ backend/routes/message.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { sendMessage } from '../controllers/messages.controller.js';
+import { deleteMessage, getMessages, sendMessage } from '../controllers/messages.controller.js';
 
 const messageRouter = express.Router();
 
 messageRouter.post("/sendMessage/:id", sendMessage);
-
+messageRouter.get("/getMessage/:id", getMessages);
+messageRouter.delete("/deleteMessage/:messageId", deleteMessage);
 export default messageRouter;
