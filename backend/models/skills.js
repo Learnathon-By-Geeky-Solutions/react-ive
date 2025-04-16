f76f300ba5401e60e85323501c7c72db2f1d97f4
tashrif-007 <tashrifpro@gmail.com>
2025-04-16 11:13:06 +0600

    updated post and subject model

--- backend/models/skills.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const skillSchema = new mongoose.Schema({
-    name: { type: String, unique: true, required: true }, 
-});
-
-const Skill = mongoose.model('Skill', skillSchema);
-export default Skill;
