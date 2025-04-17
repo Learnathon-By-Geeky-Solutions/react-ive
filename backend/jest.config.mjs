1e27f7322bb5e6df983a5902bdf3eb6fc086a540
tashrif-007 <tashrifpro@gmail.com>
2025-04-17 13:41:47 +0600

    updated ci and sonar to include tests coverage

--- backend/jest.config.mjs
+++ backend/jest.config.mjs
@@ -4,6 +4,6 @@ export default {
   },
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['lcov', 'text'],
 };
