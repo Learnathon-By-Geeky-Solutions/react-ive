597dd4750e2ead7273e3b3e72a7cc807225d3a86
tashrif-007 <tashrifpro@gmail.com>
2025-04-20 23:16:51 +0600

    init frontend testing

---
+++ frontend/src/setupTests.js
@@ -0,0 +1,10 @@
+// setupTests.js
+import '@testing-library/jest-dom';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+
+  
+import { TextEncoder, TextDecoder } from 'util';
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
