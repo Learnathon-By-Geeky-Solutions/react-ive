370dcafa6b1daa0e885cf60da679bcad04b0170a
tashrif-007 <tashrifpro@gmail.com>
2025-04-25 20:22:44 +0600

    added prop validation in authlayout and googleauthbutton

--- frontend/src/components/GoogleAuthButton.jsx
+++ frontend/src/components/GoogleAuthButton.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { GoogleButton, GoogleIcon } from './AuthStyles';
 
 const GoogleAuthButton = ({ onClick, isLoading, buttonText }) => {
@@ -13,6 +14,16 @@ const GoogleAuthButton = ({ onClick, isLoading, buttonText }) => {
       {buttonText}
     </GoogleButton>
   );
+};
+
+GoogleAuthButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  isLoading: PropTypes.bool,
+  buttonText: PropTypes.string.isRequired
+};
+
+GoogleAuthButton.defaultProps = {
+  isLoading: false
 };
 
 export default GoogleAuthButton;
