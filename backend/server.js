2e09d1754cb9c5f7eb83efff30283f7a20c65e18
tashrif-007 <tashrifpro@gmail.com>
2025-04-26 19:53:54 +0600

    added frontend url in backend

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const PORT = process.env.PORT || 3500;
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials: true,
   })
 );
