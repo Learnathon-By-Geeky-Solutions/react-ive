a2c5c6963723a35ee9ff5725103242341cb4b170
tashrif-007 <tashrifpro@gmail.com>
2025-04-30 20:11:32 +0600

    added cloudinary file upload

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,21 +1,8 @@
 import multer from "multer";
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const storage = multer.memoryStorage();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const uploadPath = path.join(__dirname, 'uploads');
-    cb(null, uploadPath); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-});
-
-// Setting file size limit to 5MB (5 * 1024 * 1024 bytes)
-export const upload = multer({ 
+export const upload = multer({
   storage,
   limits: { fileSize: 5 * 1024 * 1024 } // 5 MB
 });
